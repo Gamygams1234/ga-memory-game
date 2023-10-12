e3f4f0eb41619c6293c7a290cebd6c64f2f10b5e
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2023-10-11 21:18:56 -0700

    Setting a conditional for the end game

--- script.js
+++ script.js
@@ -9,12 +9,17 @@ const shuffleArray = (arr) => arr.sort(() => 0.5 - Math.random());
 function startGame() {
   let guesses = [];
   let moves = 0;
+  let currentPlayer = 1;
+  let playingArray = numbersArray;
+  let correctGuesses = 0; 
 
-//   shuffling the array
-  shuffleArray(numbersArray);
-//   empty the panels
+
+
+  //   shuffling the array
+  shuffleArray(playingArray);
+  //   empty the panels
   panels.innerHTML = "";
-  numbersArray.forEach((num) => {
+  playingArray.forEach((num) => {
     panels.insertAdjacentHTML(
       "beforeend",
       ` <li class="clickable" data-guess="${num}">
@@ -24,11 +29,41 @@ function startGame() {
     );
   });
 
+  function makeGuess(guess) {
+    guesses.push(guess);
+    if (guesses.length === 2) {
+      moves += 1;
+      if (guesses[0] === guesses[1]) {
+        document.querySelectorAll("li.clicked").forEach((item) => {
+          // this is for the correct guess
+          correctGuesses += 1;
+          item.classList.add("success");
+          item.classList.remove("clicked");
+          if (correctGuesses === playingArray.length){
+            alert("You win the game!")
+          }
+        });
+      } else {
+        document.querySelectorAll("li.clicked").forEach((item) => {
+          // this is for the wrong guess
+          item.classList.add("wrong");
+          setTimeout(function () {
+            item.classList.remove("active");
+            item.classList.remove("wrong");
+            item.classList.remove("clicked");
+            item.classList.add("clickable");
+          }, 500);
+        });
+      }
+      guesses.length = 0;
+    }
+  }
   document.querySelectorAll(".clickable").forEach((item) => {
     item.addEventListener("click", (e) => {
       if (e.target.parentElement.classList.contains("clickable")) {
-        e.target.parentElement.classList.toggle("active");
-        console.log(item)
+        e.target.parentElement.classList.toggle("clickable");
+        e.target.parentElement.classList.add("clicked", "active");
+        makeGuess(e.target.parentElement.dataset.guess);
       }
     });
   });
